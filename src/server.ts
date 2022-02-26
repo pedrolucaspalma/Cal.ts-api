002b74e2a9568aab1baf69d329712803deed4b5b
pedrolucaspalma <pedrolucaspalma@gmail.com>
2022-02-26 12:34:04 -0300

    update logging method to use new logger service

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,19 @@
-console.log("hello world")
+import express, { Request, Response, NextFunction }  from 'express'
+const logger = require('./services/logger')
+
+// const cors = require("cors")
+const port = process.env.PORT || '8080';
+
+const app = express();
+app.use(express.json());
+// app.use(cors())
+
+// app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+//     if (err instanceof Error) {
+//       return res.status(400).json({
+//         error: err.message,
+//       });
+//     }
+// }
+
+const server = app.listen(3000, () => logger.info("Server is running"));
