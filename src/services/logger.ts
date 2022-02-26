7e0f160cf480077fcaa4c6aca4a5be1a532ee5bc
pedrolucaspalma <pedrolucaspalma@gmail.com>
2022-02-26 12:33:51 -0300

    add logger service

---
+++ src/services/logger.ts
@@ -0,0 +1,39 @@
+const winston = require('winston')
+const fs = require('fs');
+
+let logger;
+
+if (!logger) {
+	logger = winston.createLogger({
+		level: 'info',
+		format: winston.format.combine(
+			winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+			winston.format.errors({ stack: true }),
+			winston.format.splat(),
+			winston.format.json(),
+		),
+		transports: [
+			new winston.transports.File({
+				level: 'error',
+				filename: 'logs/error.log',
+				maxsize: 100000,
+				maxFiles: 10,
+			}),
+			new winston.transports.File({
+				filename: 'logs/combined.log',
+				maxsize: 100000,
+				maxFiles: 10,
+			}),
+		],
+    })
+	logger.add(
+		new winston.transports.Console({
+			format: winston.format.combine(
+				winston.format.colorize(),
+				winston.format.simple(),
+			),
+		}),
+	);
+}
+
+module.exports = logger
